d92da1aaadf66898fa420ceebf98229e4e65ecdd
Feluan <lipeluan009@gmail.com>
2025-06-23 11:15:08 -0300

    aula de python

---
+++ aula.py
@@ -0,0 +1,20 @@
+num1 = int(input("Digite um numero:"))
+num2 = int(input("Digite outro numero:"))
+
+num3 = num1 + num2
+num4 = num1 - num2
+num5 = num1 / num2
+num6 = num1 * num2
+num7 = num1 ** num2
+num8 = num1 // num2
+num9 = num1 % num2
+
+print(f"O primeiro numero é {num1}, o segundo numero é {num2}")
+
+print(f"a soma deles é {num3}")
+print(f"a subtração deles é {num4}")
+print(f"a divisão deles é {num5}")
+print(f"a multiplicação deles é {num6}")
+print(f"a exponenciação deles é {num7}")
+print(f"a divisão inteira deles é {num8}")
+print(f"o modulo deles é {num9}")
